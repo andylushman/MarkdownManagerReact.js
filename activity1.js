2d9d04ea5173eb2deae1c149f8f1621455aadc1e
Andy Lushman <andylushman@gmailcom>
2017-12-19 10:11:42 -0700

    Activity 1 solution

---
+++ activity1.js
@@ -0,0 +1,11 @@
+function getElementsByAttribute(attribute, value) {
+  var all = document.getElementsbyTagName("*");
+  var found = [];
+
+  for (var i = 0; i < all.length; i++) {
+    element = all[i];
+    if (all[i].getAttribute(attribute) === value) {
+      found.push(all[i]);
+    }
+  }
+};
