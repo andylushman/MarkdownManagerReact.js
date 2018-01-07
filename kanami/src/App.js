aa4c17e1e9a95d7b3f32e1d87ea4d184a7689a8b
Andy Lushman <andylushman@gmail.com>
2018-01-07 09:39:15 -0700

    Added Konami project

---
+++ kanami/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Konami from "./components/Konami"
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activated: false,
+      name: ''
+    };
+    this.toggleActivation = this.toggleActivation.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleActivation() {
+    //console.log(this);
+    if (this.state.activated === true) {
+      this.setState({activated: false });
+    } else {
+      this.setState({activated: true });
+    }
+  }
+
+  handleChange(e){
+    console.log(e.target.value);
+    this.setState({name: e.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome {this.state.name.length > 1 ? `, ${this.state.name}` : ``}</h1>
+        </header>
+
+        <input type= 'text' onChange= {this.handleChange} placeholder='Your Name' />
+
+        <div className= 'konami'>
+          <button onClick={this.toggleActivation}> Toggle Konami Code! </button>
+          {this.state.activated ? <Konami name ={this.state.name}/> : <div> Konami Code: Deactivated</div>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
